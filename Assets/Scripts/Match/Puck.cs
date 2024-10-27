f3d06d6c23d1545b70a582dc7eec8bc4206389ba
Matheus Amazonas <matheus.amazonas@gmail.com>
2024-10-27 17:25:50 +0100

    ⬆️ Upgrade to Unity 6.0.24
    There are compilation errors caused by UniTask that will be solved with a plugin upgrade in a separate commit.

--- Assets/Scripts/Match/Puck.cs
+++ Assets/Scripts/Match/Puck.cs
@@ -32,7 +32,7 @@ namespace LazySquirrelLabs.AirHockey.Match
 		/// <param name="position"></param>
 		internal void Regroup(Vector2 position)
 		{
-			_rigidbody2D.velocity = Vector2.zero;
+			_rigidbody2D.linearVelocity = Vector2.zero;
 			_rigidbody2D.position = position;
 			_rigidbody2D.angularVelocity = 0f;
 			_rigidbody2D.rotation = 0f;
@@ -44,7 +44,7 @@ namespace LazySquirrelLabs.AirHockey.Match
 		internal void StopMoving()
 		{
 			_rigidbody2D.isKinematic = true;
-			_rigidbody2D.velocity = Vector2.zero;
+			_rigidbody2D.linearVelocity = Vector2.zero;
 			_rigidbody2D.angularVelocity = 0f;
 		}
 
